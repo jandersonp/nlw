68cbd965dcc00199e09bd91150e15efc297951d8
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2020-06-02 21:47:01 -0300

    configurando conexão com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
 
-  response.json({message: 'Hello World'})
-});
-
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
 
-export default app;
